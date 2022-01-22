1db38ffa236fed4bde2594a2f8e848c0506e7e22
Bikatti <ibikatti@gmail.com>
2022-01-22 21:48:57 +0000

    project: finish

--- src/index.js
+++ src/index.js
@@ -1,67 +1,73 @@
-const Web3 = require('web3');
+const Web3 = require('web3')
 
-window.onload = () => {
-  // Variables
-  let web3;
-  let from;
+window.onload = function () {
+  let web3
+  let from
 
   // Elements
-  const connectButton = document.getElementById('connect');
-  const content = document.getElementById('content');
-  const account = document.getElementById('account');
+  const connectBtn = document.getElementById('connect')
+  const content = document.getElementById('content')
+  const account = document.getElementById('account')
 
   // Form
-  const form = document.getElementById('send');
-  const amountInput = document.getElementById('amount');
-  const recipientInput = document.getElementById('recipient');
+  const form = document.getElementById('send')
+  const recipientInput = document.getElementById('recipient')
+  const amountInput = document.getElementById('amount')
+
+  const ethereum = window.ethereum
 
   // Functions
   const connect = async () => {
-    if (window.ethereum) {
-      web3 = new Web3(window.ethereum);
-
+    if (ethereum) {
       try {
-        await window.ethereum.request({ method: 'eth_requestAccounts' });
-
-        content.style.display = 'initial';
-        connectButton.style.display = 'none';
-
-        const accounts = await web3.eth.getAccounts();
-
-        from = accounts[0];
-
-        account.innerText = from;
-      } catch (err) {
-        alert('Please accept the request');
+        await ethereum.request({
+          method: 'eth_requestAccounts'
+        })
+  
+        web3 = new Web3(Web3.givenProvider || ethereum)      
+  
+        let accounts = await web3.eth.getAccounts()
+  
+        from = accounts[0]
+  
+        content.style.display = 'initial'
+        connectBtn.style.display = 'none'
+        account.innerText = from
+      } catch (error) {
+        alert('Has rechazado la conexión')
       }
-    } else {
-      alert('Web3 is required');
     }
-  };
 
-  const transact = async (event) => {
-    event.preventDefault();
-    const amount = amountInput.value;
-    const recipient = recipientInput.value;
+    
+    if (!ethereum) {
+      alert('Necesitas un proveedor de web3')
+    }
+  }
+
+  const transact = async event => {
+    event.preventDefault()
+    
+    const amount = amountInput.value
+    const recipient = recipientInput.value
+
+    if(Number(amount) <= 0) {
+      alert('Valor no permitido')
+      return
+    }
 
     if (!web3.utils.isAddress(recipient)) {
-      alert('Dirección inválida');
-      return;
-    }
-
-    if (Number(amount) <= 0) {
-      alert('Cantidad inválida');
-      return;
+      alert('Dirección inválida')
+      return
     }
 
     web3.eth.sendTransaction({
-      from,
+      from: from,
       to: recipient,
       value: amount,
-    });
-  };
+    })
+  }
 
   // Listeners
-  connectButton.onclick = connect;
-  form.onsubmit = transact;
-};
+  connectBtn.onclick = connect
+  form.onsubmit = transact
+}
